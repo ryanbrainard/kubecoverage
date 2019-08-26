6c807e8db54ce6379406db7fe4b0ff28bbac0a46
Ryan Brainard <966764+ryanbrainard@users.noreply.github.com>
2019-08-26 09:27:25 +0900

    wip

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,8 @@ WORKDIR /app
 COPY . .
 
 RUN go get -d -v ./...
-RUN go test -c -o external_test
+RUN go test -c -o external_test -test.coverprofile=/coverage/out
 
 EXPOSE 8080
 
-ENTRYPOINT exec /app/external_test
+ENTRYPOINT exec /app/external_test -test.coverprofile=/coverage/out -test.v
--- external_test.sh
+++ external_test.sh
@@ -1,7 +1,6 @@
-docker build -t external-test .
 kubectl delete -f external_test_k8s.yaml
-kubectl replace -f external_test_k8s.yaml
-kail -l run=external-test &
-sleep 2
-kubectl delete pod -l run=external-test
-kail -l run=external-test
+docker build -t external-test .
+kubectl apply -f external_test_k8s.yaml
+#kail -l run=external-test &
+#sleep 2c
+#kubectl delete pod -l run=external-test

ba1f6568352eccd53c3b73c4b5a95dfc4e584dcb
Ryan Brainard <966764+ryanbrainard@users.noreply.github.com>
2019-08-26 18:24:55 +0900

    Await port forwarding

--- demo.sh
+++ demo.sh
@@ -1,3 +1,18 @@
+function kill_port_forward() {
+  pkill kubectl -n go-test-coverage-demo port-forward
+}
+
+function await_port_forward() {
+  kill_port_forward
+  out=/tmp/port-fwd-out
+  rm $out
+  touch $out
+  kubectl port-forward -n go-test-coverage-demo --pod-running-timeout=1m0s "$@" > $out &
+  tail -f $out | grep -m 1 'Forwarding from'
+}
+
+trap kill_port_forward EXIT
+
 # Build special test binary, package as an image, and push to registry
 docker build -t ryanbrainard/go-test-coverage-demo-test-exec .
 docker push ryanbrainard/go-test-coverage-demo-test-exec
@@ -7,8 +22,7 @@ kubectl delete namespace go-test-coverage-demo
 kubectl apply -f demo.yaml
 
 # Run the test
-kubectl -n go-test-coverage-demo port-forward --pod-running-timeout=10s service/test-exec 30001:http &
-sleep 10
+await_port_forward service/test-exec 30001:http
 curl 'http://localhost:30001/?size=1'
 curl 'http://localhost:30001/?size=100'
 
@@ -16,7 +30,6 @@ curl 'http://localhost:30001/?size=100'
 kubectl -n go-test-coverage-demo delete pod -l app=test-exec
 
 # Download output
-kubectl -n go-test-coverage-demo port-forward --pod-running-timeout=10s service/coverage-viewer 30002:http &
-sleep 10
-curl -O 'http://localhost:30002/tmp/coverage/demo.cov'
+await_port_forward service/test-exec 30001:http
+curl -s -O 'http://localhost:30002/tmp/coverage/demo.cov'
 go tool cover -html demo.cov
